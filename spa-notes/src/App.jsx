61751135a0a1bd945bc8b2877a0af5069b2b4783
Kyle Fluto <kyle.fluto@gmail.com>
2025-04-03 17:28:47 -0500

    Finish lesson 2 section d

--- spa-notes/src/App.jsx
+++ spa-notes/src/App.jsx
@@ -1,33 +1,95 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then((response) => {
-        console.log('promise fulfilled');
-        setNotes(response.data);
+  // Update (PUT)
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(note => note.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote));
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content} was already deleted from the server`
+        )
+        setNotes(notes.filter(note => note.id !== id))
       });
   }
-  
-  useEffect(hook, []);
+
+  // Read (GET)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      });
+  }, []);
+
+  // Create (POST)
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      });
+  }
+
+  const handleShowFilter = (e) => {
+    if (e.target.innerText === "show all") {
+      setShowAll(true);
+    } else {
+      setShowAll(false);
+    }
+  }
+
+  const renderAllNotes = () => {
+    return (
+      <ul>
+        {notes.map((note) => {
+          return <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
+        })}
+      </ul>
+    )
+  }
+
+  const renderImportantNotes = () => {
+    const importantNotes = notes.filter(note => note.important === true);
+    return (
+      <ul>
+      {importantNotes.map((importantNote) => {
+        return <Note key={importantNote.id} note={importantNote} toggleImportance={() => toggleImportanceOf(importantNote.id)}/>
+      })}
+    </ul>
+    )
+  }
 
 
   return (
     <div>
       <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      {notes.length === 0 && <p>No notes found</p>}
+      {showAll ? renderAllNotes() : renderImportantNotes()}
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={(e) => setNewNote(e.target.value)} />
+        <button type="submit">Add Note</button>
+        <button type="button" onClick={handleShowFilter}>{showAll ? "show important" : "show all"}</button>
+      </form>
     </div>
   );
 };
--- spa-notes/src/components/Note.jsx
+++ spa-notes/src/components/Note.jsx
@@ -1,5 +1,14 @@
-const Note = ({ note }) => {
-  return <li>{note.content}</li>;
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make unimportant' : 'make important';
+  
+  
+  return (
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
 };
 
 export default Note;
